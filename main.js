80fafb26c1ea7abe28ac2696994addf1d8dff83b
Rakhmanov-03 <omadjonrahmonov588@gmail.com>
2023-11-14 21:27:24 +0500

    4.5-dars_vazifa

---
+++ main.js
@@ -0,0 +1,85 @@
+// for1
+// let k=+prompt("k ni kiriting")
+// let n=+prompt("n ni kiriting")
+// for(let i=0; i<n; i++){
+//     console.log(k);
+// }
+// for2
+// let a=+prompt("a ni kiriting")
+// let b=+prompt("b ni kiriting")
+// for (let i=1; i<=b; i++){
+//     console.log(i);
+// }
+// for3
+// let a=+prompt("a ni kiriting")
+// let b=+prompt("b ni kiriting")
+// for (let i=b-1; i>1; i--){
+//     console.log(i);
+// }
+// for4
+// let narx=+prompt("Narxni kiriting")
+// for (let i=1; i<=10; i++){
+//     console.log(i*narx);
+// }
+// for5
+// let narx=+prompt("Narxni kiriting")
+// for (let i=0.1; i<=1; i+=0.1){
+//     console.log(i*narx);
+// }
+// for6
+// let narx=+prompt("Narxni kiriting")
+// for(let i=1.2; i<=2; i+=0.2){
+//     console.log(i*narx);
+// }
+// for7
+// let a=+prompt("a ni kiriting")
+// let b=+prompt("b ni kiriting")
+// let son=0
+// for (let i=1; i<=b; i++){
+//     console.log(son=son+i);
+// }
+// for8
+// let a=+prompt("a ni kiriting")
+// let b=+prompt("b ni kiriting")
+// let son=1
+// for (let i=1; i<=b; i++){
+//      console.log(son=son*i);
+//      }
+// for9
+// let a=+prompt("a ni kiriting")
+// let b=+prompt("b ni kiriting")
+// let son=0
+// for (let i=1; i<=b; i++){
+//      console.log(son=son+i**2);
+//      }
+// for10
+// let son=0
+// let n=+prompt("n ni kiriting")
+// for(let i=1; i<=n; i++){
+//     son+=1/i
+// }
+// console.log(son);
+// for12
+// let n=+prompt("n ni kiriting")
+// son=1
+// x=1.1
+// for(i=1; i<n; i++){
+// x=x+0.1
+// son=son*x
+// }
+// console.log(son*1.1);
+// while1
+// let a=+prompt("a ni kiriting")
+// let b=+prompt("b ni kiriting")
+// while(a>b){
+//     console.log(a=a%b);
+//     a++;
+// }
+// while5
+let n=+prompt("n ni kiriting")
+let k=1
+while(1<=n){
+    if(k=n);
+    n++
+}
+console.log(k);
